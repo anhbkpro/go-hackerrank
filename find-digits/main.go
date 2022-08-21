725360f380fc561f83196384691383891ed866d5
Anh Lai <laingocanh1990@gmail.com>
2022-08-21 10:47:25 +0700

    find digits

---
+++ find-digits/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+func findDigits(n int32) int32 {
+	nStr := fmt.Sprint(n)
+	runes := []rune(nStr)
+	divisorNum := int32(0)
+	for _, num := range runes {
+		divisor, err := strconv.Atoi(string(num))
+		if err != nil || divisor == 0 {
+			continue
+		}
+		if n%int32(divisor) == 0 {
+			divisorNum++
+		}
+	}
+
+	return divisorNum
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+	writer := bufio.NewWriterSize(os.Stdin, 16*1024*1024)
+
+	tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		result := findDigits(n)
+
+		fmt.Fprintf(writer, "%d\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
