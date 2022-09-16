bb437a5b0bc8eefd74166548dc1a5fab840486b3
Anh Lai <laingocanh1990@gmail.com>
2022-09-16 16:04:34 +0700

    Counting Sort 1

---
+++ one-month-preparation-kit/countingsort1/countingsort1.go
@@ -0,0 +1,103 @@
+package countingsort1
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'CountingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func CountingSort(arr []int32) []int32 {
+	m := make(map[int32]int32)
+	min := int32(math.MaxInt32)
+	max := int32(0)
+	for _, v := range arr {
+		if max < v {
+			max = v
+		}
+		if min > v {
+			min = v
+		}
+		if _, ok := m[v]; ok {
+			m[v]++
+		} else {
+			m[v] = 1
+		}
+	}
+	fmt.Println("max =", max)
+	fmt.Println("min =", min)
+	var result []int32
+	for i := int32(0); i < 100; i++ {
+		if v, ok := m[i]; ok {
+			result = append(result, v)
+		} else {
+			result = append(result, 0)
+		}
+	}
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := CountingSort(arr)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
