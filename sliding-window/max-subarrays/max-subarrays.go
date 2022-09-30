bf50552328138855eaf9fa6fa784530544082f72
Anh Lai <laingocanh1990@gmail.com>
2022-09-30 09:29:23 +0700

    refactor code

--- sliding-window/max-subarrays/max-subarrays.go
+++ sliding-window/max-subarrays/max-subarrays.go
@@ -1,18 +1,17 @@
 package max_subarrays
 
+import "math"
+
 func MaxSumSubArrayOfSizeK(k int32, arr []int32) int32 {
-	var result int32
-	var windowStart int32
-	var windowSum int32
+	var result, windowStart, windowSum int32
 	for windowEnd := int32(0); windowEnd < int32(len(arr)); windowEnd++ {
 		windowSum += arr[windowEnd]
 		if windowEnd >= k-1 {
-			if windowSum > result {
-				result = windowSum
-			}
+			result = int32(math.Max(float64(result), float64(windowSum)))
 			windowSum -= arr[windowStart]
 			windowStart++
 		}
 	}
+
 	return result
 }
