511cfa775a5a4bac425922b08669c1e6d55a11c7
Anh Lai <laingocanh1990@gmail.com>
2022-10-29 00:05:38 +0700

    [Sliding Window] 424. Longest Repeating Character Replacement

--- leetcode/sliding-window/longest-repeating-character-replacement/main.go
+++ leetcode/sliding-window/longest-repeating-character-replacement/main.go
@@ -10,17 +10,14 @@ func CharacterReplacement(s string, k int) int {
 		return len(s)
 	}
 
-	l, ans, maxFreq := 0, 0, 0
+	l, ans, mostFreq := 0, 0, 0
 	freqWindow := make(map[string]int)
 	for r := 0; r < len(s); r++ {
 		cRight := string(s[r])
-		if _, ok := freqWindow[cRight]; ok {
-			freqWindow[cRight]++
-		} else {
-			freqWindow[cRight] = 1
-		}
-		maxFreq = int(math.Max(float64(maxFreq), float64(freqWindow[cRight])))
-		if r-l+1-maxFreq > k {
+		freqWindow[cRight]++
+		mostFreq = int(math.Max(float64(mostFreq), float64(freqWindow[cRight])))
+
+		if r-l+1-mostFreq > k {
 			cLeft := string(s[l])
 			freqWindow[cLeft]--
 			if freqWindow[cLeft] == 0 {
