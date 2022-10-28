4100c232599b96c7cf0eaf1e2fde52497453ac4e
Anh Lai <laingocanh1990@gmail.com>
2022-10-29 00:34:39 +0700

    use for loop to maintain right pointer/index

--- leetcode/sliding-window/minimum-size-subarray-sum/main.go
+++ leetcode/sliding-window/minimum-size-subarray-sum/main.go
@@ -3,15 +3,14 @@ package minimum_size_subarray_sum
 import "math"
 
 func MinSubArrayLen(target int, nums []int) int {
-	l, r, min, windowSum := 0, 0, math.MaxInt, 0
-	for r < len(nums) {
+	l, min, windowSum := 0, math.MaxInt, 0
+	for r := 0; r < len(nums); r++ {
 		windowSum += nums[r]
 		for windowSum >= target {
-			windowSum -= nums[l]
 			min = int(math.Min(float64(min), float64(r-l+1)))
+			windowSum -= nums[l]
 			l++
 		}
-		r++
 	}
 	if min == math.MaxInt {
 		return 0
