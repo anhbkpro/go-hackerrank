c01ebb0244f8dd068c8c13dfc5543480b1a368c5
Anh Lai <laingocanh1990@gmail.com>
2022-11-05 15:48:15 +0700

    [String] 1513. Number of Substrings With Only 1s

--- leetcode/string/number-of-substrings-with-only-1s/main.go
+++ leetcode/string/number-of-substrings-with-only-1s/main.go
@@ -4,21 +4,15 @@ const MOD = 1000000007
 
 // NumSub Similar questions: https://leetcode.com/problems/count-number-of-homogenous-substrings/
 func NumSub(s string) int {
-	ans := 0
-	var cur rune
-	runes := []rune(s)
 	count := 0
+	ans := 0
+	runes := []rune(s)
 	for i := 0; i < len(runes); i++ {
-		if string(runes[i]) == "0" {
-			count = 0
-			continue
-		}
-		if runes[i] == cur {
+		if string(runes[i]) == "1" {
 			count++
 		} else {
-			count = 1
+			count = 0
 		}
-		cur = runes[i]
 		ans = (ans + count) % MOD
 	}
 	return ans
