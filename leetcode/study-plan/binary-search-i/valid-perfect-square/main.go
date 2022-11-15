b01ce40810d216dde715f8728cac8239a92102dd
Anh Lai <laingocanh1990@gmail.com>
2022-11-15 19:08:23 +0700

    refer naming in go package

--- leetcode/study-plan/binary-search-i/valid-perfect-square/main.go
+++ leetcode/study-plan/binary-search-i/valid-perfect-square/main.go
@@ -5,18 +5,16 @@ package valid_perfect_square
 //	Runtime: 0 ms, faster than 100.00% of Go online submissions for Valid Perfect Square.
 //	Memory Usage: 1.9 MB, less than 86.52% of Go online submissions for Valid Perfect Square.
 func IsPerfectSquare(num int) bool {
-	if num == 1 {
-		return true
-	}
-	lo, hi := 0, num
-	for lo < hi {
-		mid := (lo + hi) >> 1
-		if mid*mid < num {
-			lo = mid + 1
-		} else if mid*mid > num {
-			hi = mid
-		} else {
+	i, j := 0, num
+	for i <= j {
+		h := (i + j) >> 1 // avoid overflow when computing h. Refer naming src/sort/search.go
+		if h*h == num {
 			return true
+		}
+		if h*h < num {
+			i = h + 1
+		} else {
+			j = h - 1
 		}
 	}
 	return false
