1d6c06fbdff80b8898509653a099824fd3d2851f
Anh Lai <laingocanh1990@gmail.com>
2022-11-17 11:32:26 +0700

    [Binary Search] 162. Find Peak Element

--- leetcode/binary-search/find-peak-element/main.go
+++ leetcode/binary-search/find-peak-element/main.go
@@ -3,6 +3,9 @@ package find_peak_element
 // FindPeakElement My Solution
 //	Runtime: 7 ms, faster than 40.21% of Go online submissions for Find Peak Element.
 //	Memory Usage: 2.8 MB, less than 70.98% of Go online submissions for Find Peak Element.
+// Second run:
+// 	Runtime: 0 ms, faster than 100.00% of Go online submissions for Find Peak Element.
+//	Memory Usage: 2.9 MB, less than 70.98% of Go online submissions for Find Peak Element.
 func FindPeakElement(nums []int) int {
 	lo, hi := 0, len(nums)-1
 	for lo < hi {
