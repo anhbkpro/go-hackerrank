245572e161a38d47ab37ebc1961127906a991e4b
Anh Lai <laingocanh1990@gmail.com>
2022-11-20 11:46:52 +0700

    [Binary Search] 1539. Kth Missing Positive Number

---
+++ leetcode/binary-search/kth-missing-positive-number/main.go
@@ -0,0 +1,30 @@
+package kth_missing_positive_number
+
+import "fmt"
+
+func findKthPositive(arr []int, k int) int {
+	m := make(map[int]bool)
+	for _, item := range arr {
+		m[item] = true
+	}
+	fmt.Println(m)
+	missingArr := make([]int, 0)
+	for i := 1; i <= arr[len(arr)-1]+k+1; i++ {
+		if !m[i] {
+			missingArr = append(missingArr, i)
+		}
+	}
+	fmt.Println(missingArr)
+	lo, hi := 0, len(missingArr)-1
+	for lo <= hi {
+		mid := (lo + hi) >> 1
+		if mid == k {
+			return missingArr[mid-1]
+		} else if mid > k {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+	return 0
+}
---
+++ leetcode/binary-search/kth-missing-positive-number/main_test.go
@@ -0,0 +1,33 @@
+package kth_missing_positive_number
+
+import "testing"
+
+func Test_findKthPositive(t *testing.T) {
+	type args struct {
+		arr []int
+		k   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "first",
+			args: args{arr: []int{2, 3, 4, 7, 11}, k: 5},
+			want: 9,
+		},
+		{
+			name: "second",
+			args: args{arr: []int{1, 2, 3, 4}, k: 2},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findKthPositive(tt.args.arr, tt.args.k); got != tt.want {
+				t.Errorf("findKthPositive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
