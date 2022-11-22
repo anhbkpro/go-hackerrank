690755ac6a66001c4dc80bd4b3c8af6fff496468
Anh Lai <laingocanh1990@gmail.com>
2022-11-22 10:18:32 +0700

    [Three Pointers] 1213. Intersection of Three Sorted Arrays

--- leetcode/binary-search/intersection-of-three-sorted-arrays/main.go
+++ leetcode/binary-search/intersection-of-three-sorted-arrays/main.go
@@ -31,3 +31,26 @@ func arraysIntersection(arr1 []int, arr2 []int, arr3 []int) []int {
 	return ans
 }
 
+//Runtime: 4 ms, faster than 100.00% of Go online submissions for Intersection of Three Sorted Arrays.
+//Memory Usage: 4.1 MB, less than 85.00% of Go online submissions for Intersection of Three Sorted Arrays.
+func arraysIntersection3Pointers(arr1 []int, arr2 []int, arr3 []int) []int {
+	p1, p2, p3 := 0, 0, 0
+	ans := make([]int, 0)
+	for p1 < len(arr1) && p2 < len(arr2) && p3 < len(arr3) {
+		if arr1[p1] == arr2[p2] && arr2[p2] == arr3[p3] {
+			ans = append(ans, arr1[p1])
+			p1++
+			p2++
+			p3++
+		} else {
+			if arr1[p1] < arr2[p2] {
+				p1++
+			} else if arr2[p2] < arr3[p3] {
+				p2++
+			} else {
+				p3++
+			}
+		}
+	}
+	return ans
+}
--- leetcode/binary-search/intersection-of-three-sorted-arrays/main_test.go
+++ leetcode/binary-search/intersection-of-three-sorted-arrays/main_test.go
@@ -16,11 +16,19 @@ func Test_arraysIntersection(t *testing.T) {
 		args args
 		want []int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "3 pointers",
+			args: args{
+				arr1: []int{1, 2, 3, 4, 5},
+				arr2: []int{1, 2, 5, 7, 9},
+				arr3: []int{1, 3, 4, 5, 8},
+			},
+			want: []int{1, 5},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := arraysIntersection(tt.args.arr1, tt.args.arr2, tt.args.arr3); !reflect.DeepEqual(got, tt.want) {
+			if got := arraysIntersection3Pointers(tt.args.arr1, tt.args.arr2, tt.args.arr3); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("arraysIntersection() = %v, want %v", got, tt.want)
 			}
 		})
