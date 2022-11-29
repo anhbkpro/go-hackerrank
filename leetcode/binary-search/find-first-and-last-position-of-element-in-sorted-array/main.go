b7238774bff167a311f3b804402182f74b9ed7a9
Anh Lai <laingocanh1990@gmail.com>
2022-11-29 11:01:43 +0700

    [Binary Search] 34. Find First and Last Position of Element in Sorted Array

--- leetcode/binary-search/find-first-and-last-position-of-element-in-sorted-array/main.go
+++ leetcode/binary-search/find-first-and-last-position-of-element-in-sorted-array/main.go
@@ -1,48 +1,43 @@
 package find_first_and_last_position_of_element_in_sorted_array
 
-import "fmt"
-
-//Runtime: 11 ms, faster than 71.83% of Go online submissions for Find First and Last Position of Element in Sorted Array.
+//Runtime: 12 ms, faster than 68.52% of Go online submissions for Find First and Last Position of Element in Sorted Array.
 //Memory Usage: 4 MB, less than 14.67% of Go online submissions for Find First and Last Position of Element in Sorted Array.
 func searchRange(nums []int, target int) []int {
-	if len(nums) == 0 {
+	firstOccurrence := findBound(nums, target, true)
+	if firstOccurrence == -1 {
 		return []int{-1, -1}
 	}
-	firstIndex := firstOccur(nums, target)
-	fmt.Println(firstIndex)
-	if firstIndex == -1 {
-		return []int{-1, -1}
-	}
-	secondIndex := secondOccur(nums, target)
 
-	return []int{firstIndex, secondIndex}
+	lastOccurrence := findBound(nums, target, false)
+
+	return []int{firstOccurrence, lastOccurrence}
 }
 
-func firstOccur(nums []int, target int) int {
-	l, r := 0, len(nums)
-	for l < r {
+func findBound(nums []int, target int, isFirst bool) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
 		m := (l + r) >> 1
-		if nums[m] >= target {
-			r = m
-		} else {
-			l = m + 1
-		}
-		if l >= len(nums) || (l == r && nums[l] != target) {
-			return -1
-		}
-	}
-	return l
-}
-
-func secondOccur(nums []int, target int) int {
-	l, r := 0, len(nums)
-	for l < r {
-		m := (l + r) >> 1
-		if nums[m] > target {
-			r = m
-		} else {
+		if nums[m] == target {
+			if isFirst {
+				// This means we found our lower bound.
+				if m == l || nums[m-1] != target {
+					return m
+				}
+				// Search on the left side for the bound.
+				r = m - 1
+			} else {
+				// This means we found our upper bound.
+				if m == r || nums[m+1] != target {
+					return m
+				}
+				// Search on the right side for the bound.
+				l = m + 1
+			}
+		} else if nums[m] > target {
+			r = m - 1
+		} else if nums[m] < target {
 			l = m + 1
 		}
 	}
-	return l - 1
+	return -1
 }
