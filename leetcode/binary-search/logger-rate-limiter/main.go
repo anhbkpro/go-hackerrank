896b6ef6f92f3236cc47dc560d85d3a20a646c09
Anh Lai <laingocanh1990@gmail.com>
2022-12-01 15:43:49 +0700

    [Binary Search] 359. Logger Rate Limiter

--- leetcode/binary-search/logger-rate-limiter/main.go
+++ leetcode/binary-search/logger-rate-limiter/main.go
@@ -3,26 +3,27 @@ package logger_rate_limiter
 // Logger Runtime: 136 ms, faster than 41.18% of Go online submissions for Logger Rate Limiter.
 //Memory Usage: 10 MB, less than 31.62% of Go online submissions for Logger Rate Limiter.
 type Logger struct {
-	messages map[string][]int
+	messages map[string]int
 }
 
 func Constructor() Logger {
 	return Logger{
-		messages: map[string][]int{},
+		messages: map[string]int{},
 	}
 }
 
 func (l *Logger) ShouldPrintMessage(timestamp int, message string) bool {
-	if _, ok := l.messages[message]; ok {
-		if timestamp-l.messages[message][len(l.messages[message])-1] >= 10 {
-			l.messages[message] = append(l.messages[message], timestamp)
-			return true
-		}
-	} else {
-		l.messages[message] = make([]int, 0)
-		l.messages[message] = append(l.messages[message], timestamp)
+	if _, ok := l.messages[message]; !ok {
+		l.messages[message] = timestamp
 		return true
 	}
+
+	oldTimestamp := l.messages[message]
+	if timestamp-oldTimestamp >= 10 {
+		l.messages[message] = timestamp
+		return true
+	}
+
 	return false
 }
 
