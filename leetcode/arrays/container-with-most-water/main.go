c573c559083ab8b2eb90a514bba933317a20acc1
Anh Lai <laingocanh1990@gmail.com>
2022-12-03 21:34:00 +0700

    use custom min max for shorter code

--- leetcode/arrays/container-with-most-water/main.go
+++ leetcode/arrays/container-with-most-water/main.go
@@ -1,13 +1,11 @@
 package container_with_most_water
 
-import "math"
-
 func MaxArea(height []int) int {
 	result := 0
 	l, r := 0, len(height)-1
 	for l < r {
-		area := (r - l) * int(math.Min(float64(height[l]), float64(height[r])))
-		result = int(math.Max(float64(result), float64(area)))
+		area := (r - l) * min(height[l], height[r])
+		result = max(result, area)
 		if height[l] < height[r] {
 			l++
 		} else {
@@ -16,4 +14,18 @@ func MaxArea(height []int) int {
 	}
 
 	return result
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
