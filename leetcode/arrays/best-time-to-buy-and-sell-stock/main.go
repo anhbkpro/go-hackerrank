5fc26bff08aa880536356f9d67acfbefad95c85e
Anh Lai <laingocanh1990@gmail.com>
2022-12-03 21:32:50 +0700

    use custom min max for shorter code

--- leetcode/arrays/best-time-to-buy-and-sell-stock/main.go
+++ leetcode/arrays/best-time-to-buy-and-sell-stock/main.go
@@ -1,18 +1,23 @@
 package best_time_to_buy_and_sell_stock
 
-import "math"
-
 func MaxProfit(prices []int) int {
 	l, r := 0, 1 //l = buy, right = sell
 	maxP := 0
 	for r < len(prices) {
 		if prices[l] < prices[r] {
 			profit := prices[r] - prices[l]
-			maxP = int(math.Max(float64(maxP), float64(profit)))
+			maxP = max(maxP, profit)
 		} else {
 			l = r
 		}
 		r++
 	}
 	return maxP
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
