98a3699d4464ba178c521ed9849d674ba4b986c9
Anh Lai <laingocanh1990@gmail.com>
2023-01-11 19:44:36 +0700

    55. Jump Game - Dynamic Programming

--- leetcode/dynamic-programming/jump-game/main.go
+++ leetcode/dynamic-programming/jump-game/main.go
@@ -1,1 +1,40 @@
 package jump_game
+
+import "math"
+
+func CanJump(nums []int) bool {
+	var maxJump int
+	if len(nums) == 1 {
+		return true
+	}
+	if nums[0] == 0 {
+		return false
+	}
+	for i := 0; i < len(nums)-1; i++ {
+		if i+nums[i] >= len(nums)-1 {
+			return true
+		}
+		min := int(math.Min(float64(i+nums[i]), float64(len(nums)-1)))
+		if maxJump < min {
+			maxJump = min
+		}
+		if nums[i] == 0 && i >= maxJump {
+			return false
+		}
+	}
+	return false
+}
+
+func CanJumpGreedy(nums []int) bool {
+	goal := len(nums) - 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		if i+nums[i] >= goal {
+			goal = i
+		}
+	}
+	if goal > 0 {
+		return false
+	}
+
+	return true
+}
--- leetcode/dynamic-programming/jump-game/main_test.go
+++ leetcode/dynamic-programming/jump-game/main_test.go
@@ -1,1 +1,14 @@
 package jump_game
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCanJump(t *testing.T) {
+	assert.Equal(t, false, CanJumpGreedy([]int{1, 0, 1, 0}))
+	assert.Equal(t, true, CanJumpGreedy([]int{0}))
+	assert.Equal(t, false, CanJumpGreedy([]int{0, 2, 3}))
+	assert.Equal(t, true, CanJumpGreedy([]int{2, 3, 1, 1, 4}))
+	assert.Equal(t, false, CanJumpGreedy([]int{3, 2, 1, 0, 4}))
+}
