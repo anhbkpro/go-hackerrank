a9c888cd99d6b613920c42b10d813c7c1344c894
Anh Lai <laingocanh1990@gmail.com>
2023-01-11 19:45:44 +0700

    62. Unique Paths - Dynamic Programming

--- leetcode/dynamic-programming/unique-paths/main.go
+++ leetcode/dynamic-programming/unique-paths/main.go
@@ -1,1 +1,20 @@
 package unique_paths
+
+func UniquePaths(m int, n int) int {
+	row := make([]int, n)
+	for i := 0; i < n; i++ {
+		row[i] = 1
+	}
+	for i := 0; i < m-1; i++ {
+		newRow := make([]int, n)
+		for j := 0; j < n; j++ {
+			newRow[j] = 1
+		}
+		for k := n - 2; k >= 0; k-- {
+			newRow[k] = newRow[k+1] + row[k]
+		}
+		row = newRow
+	}
+
+	return row[0]
+}
--- leetcode/dynamic-programming/unique-paths/main_test.go
+++ leetcode/dynamic-programming/unique-paths/main_test.go
@@ -1,1 +1,11 @@
 package unique_paths
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUniquePaths(t *testing.T) {
+	assert.Equal(t, 3, UniquePaths(3, 2))
+	assert.Equal(t, 28, UniquePaths(3, 7))
+}
